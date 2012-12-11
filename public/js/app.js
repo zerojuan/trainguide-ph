922a6b3c3376e361f20f496e320cd3ae7204f24f
zerojuan <juliuscebreros@gmail.com>
2012-12-11 18:40:11 +0800

    added sidebar with image
    -- minimalist gmap UI

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,22 @@ $(document).ready(function($) {
 			elementType:"geometry",
 			stylers:[{hue:"#8800ff"},{lightness:100}]
 		},{
+		    featureType: "transit.station",
+		    stylers: [
+		      { visibility: "off" }
+		    ]
+		},{
+		    featureType: "transit.station.rail",
+		    stylers: [
+		      { visibility: "on" }
+		    ]
+		},{
+		    featureType: "transit.station",
+		    elementType: "labels.icon",
+		    stylers: [
+		      { visibility: "off" }
+		    ]
+		},{
 			featureType:"road",
 			stylers:[{visibility:"on"},{hue:"#91ff00"},{saturation:-62},{gamma:1.98},{lightness:45}]
 		},{
@@ -26,22 +42,43 @@ $(document).ready(function($) {
 		},{
 			featureType:"administrative.neighborhood",
 			elementType:"labels.text",
-			stylers:[{visibility:"on"},{saturation:33},{lightness:20}]
+			stylers:[{visibility:"off"},{saturation:33},{lightness:20}]
 		}];
 
 	var mapOptions = {
         center: new google.maps.LatLng(14.5833, 121),
         zoom: 14,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        styles: mapStyle
+        styles: mapStyle,
+        zoomControl : true,
+        zoomControlOptions : {
+        	style : google.maps.ZoomControlStyle.SMALL
+        },
+        disableDefaultUI : true
 	};
 
 	var directionsService = new google.maps.DirectionsService();
 
-	var lrt1Display = new google.maps.Polyline();
-	var lrt2Display = new google.maps.Polyline();
-	var mrt3Display = new google.maps.Polyline();
-	var pnrDisplay = new google.maps.Polyline();
+	var lrt1Display = new google.maps.Polyline({
+		strokeColor : '#fdc33c',
+		strokeOpacity : 0.9,
+		strokeWeight : 6
+	});
+	var lrt2Display = new google.maps.Polyline({
+		strokeColor : '#f28740',
+		strokeOpacity : 0.9,
+		strokeWeight : 6
+	});
+	var mrt3Display = new google.maps.Polyline({
+		strokeColor : '#5384c4',
+		strokeOpacity : 0.9,
+		strokeWeight : 6
+	});
+	var pnrDisplay = new google.maps.Polyline({
+		strokeColor : '#ad86bc',
+		strokeOpacity : 0.9,
+		strokeWeight : 6
+	});
 
 	var map = new google.maps.Map(document.getElementById("map_canvas"),
 		mapOptions);	
